ebc6af41ed4d752aca29ad7158284f4a9e2f4ca8
Niklas Klein <my.taig@gmail.com>
2014-01-19 02:55:11 +0800

    Replaced double quotes with single quotes.

--- jquery.animateNumbers.js
+++ jquery.animateNumbers.js
@@ -5,20 +5,20 @@
 		return this.each( function()
 		{
 			var self = $( this ),
-				start = parseInt( self.text().replace( /,/g, "" ) );
+				start = parseInt( self.text().replace( /,/g, '' ) );
 
 			commas = (commas === undefined) ? true : commas;
 
 			$( { value: start } ).animate( { value: stop },
 			{
 				duration: duration == undefined ? 1000 : duration,
-				easing: ease == undefined ? "swing" : ease,
+				easing: ease == undefined ? 'swing' : ease,
 				step: function()
 				{
 					self.text( Math.floor( this.value ) );
 					if( commas )
 					{
-						self.text( self.text().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, "$1," ) );
+						self.text( self.text().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,' ) );
 					}
 				},
 				complete: function()
@@ -28,7 +28,7 @@
 						self.text( stop );
 						if( commas )
 						{
-							self.text( self.text().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, "$1," ) );
+							self.text( self.text().replace( /(\d)(?=(\d\d\d)+(?!\d))/g, '$1,' ) );
 						}
 					}
 				}
